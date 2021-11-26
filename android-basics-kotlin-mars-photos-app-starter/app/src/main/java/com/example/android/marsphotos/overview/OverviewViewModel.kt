41dcf0b980e6f6a1ff24bc08b32c76922dc13d66
cristhian-ruescas <cristhian.ruescas83@gmail.com>
2021-11-26 18:34:31 -0300

    inicial

---
+++ android-basics-kotlin-mars-photos-app-starter/app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.marsphotos.overview
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.android.marsphotos.network.MarsApi
+import com.example.android.marsphotos.network.MarsApi.retrofitService
+import com.example.android.marsphotos.network.MarsPhoto
+import kotlinx.coroutines.launch
+
+
+class OverviewViewModel : ViewModel() {
+
+    // The internal MutableLiveData that stores the status of the most recent request
+    private val _status = MutableLiveData<String>()
+
+    // The external immutable LiveData for the request status
+    val status: LiveData<String> = _status
+
+    init {
+        getMarsPhotos()
+    }
+
+
+    private fun getMarsPhotos() {
+        viewModelScope.launch {
+            try {
+                val listResult = retrofitService.getPhotos()
+               _status.value = "Success: ${listResult} Mars photos retrieved"
+
+            } catch (e: Exception) {
+                _status.value = "Failure: ${e.message}"
+            }
+
+        }
+    }
+}
